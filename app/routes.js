fd78a5517c85f36f5a781cba4cc427322cc82119
Ubuntu <ubuntu@ip-172-31-46-115.us-west-2.compute.internal>
2017-04-14 03:48:41 +0000

    front page no longer shows comic covers, working on fixing routes and getting users

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,9 @@
 module.exports = function(app, passport) {
+
+  //Home Page
   app.get('/', function(req, res) {
+    //Data now in database, not json file, NEED to fix after users
+    /*
     var data = req.app.get('comicData');
     var pagePhotos = [];
     var comicTitles = [];
@@ -8,13 +12,23 @@ module.exports = function(app, passport) {
       pagePhotos = pagePhotos.concat(item.folder + '/' + item.Cover);
       comicTitles = comicTitles.concat(item.Title);
     });
+    */
 
     res.render('index', {
       pageTitle: 'Home',
-      coverArt: pagePhotos,
-      title: comicTitles,
+      //coverArt: pagePhotos,
+      //title: comicTitles,
       pageID: 'home'
     });
   });
+
+  //Login
+  app.get('/login', function(req, res) {
+    res.render('login.ejs', {message: req.flash('loginMessage') });
+  });
+
+  //process the login form
+  // app.post('/login', do passport stuff);
+
 
 };
--- app/server.js
+++ app/server.js
@@ -12,7 +12,9 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 
 var configDB = require('./config/database.js');
-var comicData = require('./data/comicData.json');
+
+//No longer used, need to get data from mongodb
+//var comicData = require('./data/comicData.json');
 
 //configuration
 mongoose.connect(configDB.url);
@@ -25,9 +27,11 @@ app.use(cookieParser()); // read cookies
 app.use(bodyParser()); /// get information from html forms
 
 app.set('port', process.env.PORT || 3000); //set port and default port
-app.set('comicData', comicData);
 app.set('view engine', 'ejs');
 app.set('views', 'app/views');
+
+//Used to give data to views and routes, no longer used
+//app.set('comicData', comicData);
 
 //required for passport
 app.use(session({ secret: 'shenanigans' }));
@@ -37,7 +41,6 @@ app.use(flash()); //use connect-flash for flash messages stored in session
 
 
 app.use(express.static('app/public'));
-//app.use(require('./routes/index'));
 
 //routes
 require('./routes.js')(app, passport); 
