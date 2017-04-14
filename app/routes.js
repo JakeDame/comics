5fb15fbd8f912028d2a5d14545f8233cdbbcdd95
JakeDame <jakedame1@gmail.com>
2017-04-14 04:42:41 +0000

    added login and signup views, along with user model including a schema

--- app/routes.js
+++ app/routes.js
@@ -30,5 +30,34 @@ module.exports = function(app, passport) {
   //process the login form
   // app.post('/login', do passport stuff);
 
+  app.get('/signup', function(req, res) {
+    res.render('signup.ejs', { message: req.flash('signupMessage') });
+  });
+
+  //process the signup form
+  // app.post('/signup', do passport stuff);
+
+  //Profile
+  app.get('/profile', isLoggedIn, function(req, res) {
+    res.render('profile,ejs', {
+      user : req.user // get the user out of session and pass to template
+    });
+  });
+
+  //Logout
+  app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
 
 };
+
+function isLoggedIn(req, res, next) {
+
+  // if user is authenticated in the session, carry on
+  if (req.isAuthenticated())
+    return next();
+
+  // if they aren't redirect them to the home page
+  res.redirect('/');
+}
